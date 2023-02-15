4e4b3496435a8d9c04d165d7e1a4b01b408df899
yoonyj1 <116607314+yoonyj1@users.noreply.github.com>
2023-02-15 09:59:41 +0900

    202302151000

---
+++ jspProject/src/com/kh/board/controller/BoardListController.java
@@ -0,0 +1,46 @@
+package com.kh.board.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BoardListController
+ */
+@WebServlet("/list.bo")
+public class BoardListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardListController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// ------------------- 페이징 처리(어려움) ---------------------
+		// 원리파악 & 공식외우기
+		
+		int listCount; // 현재 총 게시글 개수
+		int currentPage; // 현재 페이지(사용자가 요청한 페이지)
+		int pageLimit; // 페이지 하단에 보여질 페이징바의 페이지 최대 개수(몇개 단위 씩 배치할 건지)
+		int boardLimit; // 한 페이지 내에 보여질 최대 개수(몇개 단위 씩)
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
